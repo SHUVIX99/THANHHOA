9879718df2292dd4bc5f8a69d2c90f9b6f9d969d
SHUVIX99 <89714061+SHUVIX99@users.noreply.github.com>
2022-10-20 10:45:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Cảm ơn em đã đến bên anh",
+  introDesc: `Thanh Hòa 20-10 vui vẻ,\n
+    Đây là món quà thứ 3 hihi,\n
+    Hi vọng em sẽ thích\n
+    `,
+  btnIntro: "hihi,vào thôi",
+  title: "Em có muốn anh bên em mãi không?",
+  desc: "Phải chăng em đã say ngay từ lúc nghe thấy giọng nói dạ",
+  btnYes: "dạ vâng <33",
+  btnNo: "Không, anh bị đin à >.<",
+  question: "Yêu anh không <3",
+  btnReply: "Anh không biết đâu",
+  reply: "dạ vâng <3Anh thức trắng đêm vừa học làm cái trò mèo này đấy",
+  mess: "Đùa đấy, Lo mà đi chơi với người ta đi",
+  messDesc: "Anh sẽ vào đó sớm thôi, anh sẽ cạnh tranh dù em có nói gì",
+  btnAccept: "Cưng quá đi thoai <3",
+  messLink: "https://www.messenger.com/t/100007203491979", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
